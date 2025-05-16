04652a413beca9aa9d2430f7abc547a20e8d26b9
miamioh-allame <allame@miamioh.edu>
2025-05-15 20:16:24 -0400

    Update data-clear.py

--- data-clear.py
+++ data-clear.py
@@ -1,24 +1,12 @@
-import sqlite3
-
-DATABASE = '/nfs/demo.db'
-
-def connect_db():
-    """Connect to the SQLite database."""
-    return sqlite3.connect(DATABASE)
+iimport sqlite3
 
 def clear_sample_contacts():
-    """Delete only sample contacts from the database."""
-    db = connect_db()
+    db = sqlite3.connect("/nfs/demo.db")
     cursor = db.cursor()
-
-    cursor.execute('''
-        DELETE FROM contacts
-        WHERE name LIKE 'Sample Contact%'
-    ''')
-
+    cursor.execute("DELETE FROM contacts WHERE name LIKE 'Sample Contact %'")
     db.commit()
-    print('Sample contacts have been deleted from the database.')
     db.close()
+    print("Sample contacts removed.")
 
 if __name__ == '__main__':
     clear_sample_contacts()
